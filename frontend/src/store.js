8f136a7b6989bd46f1ff13b9347f26c00ceb6890
DevSamaa <samaakanani@gmail.com>
2020-01-24 16:41:48 +1100

    the data is now being passed through

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,4 +3,7 @@ import { reducer as formReducer } from "redux-form";
 const rootReducer = combineReducers({
   form: formReducer
 });
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);

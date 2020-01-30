f33b6006c9b4be58dda99425783867f3802138a0
DevSamaa <samaakanani@gmail.com>
2020-01-30 16:51:13 +1100

    made some git related changes

--- frontend/src/components/GalleryCard.js
+++ frontend/src/components/GalleryCard.js
@@ -49,3 +49,30 @@ export class GalleryCard extends Component {
 }
 
 export default GalleryCard;
+
+// import React, { Component } from "react";
+// import "../styles/GalleryPage.css";
+// import gallery1 from "../images/gallery1.jpg";
+// import gallery2 from "../images/gallery2.jpg";
+// import gallery3 from "../images/gallery3.jpg";
+// import gallery4 from "../images/gallery4.jpg";
+// import Card from "react-bootstrap/Card";
+
+// export class GalleryCard extends Component {
+//   render() {
+//     return (
+//       <div>
+
+//         <Card style={{ width: "18rem" }}>
+//           <Card.Img variant="top" src={gallery4} />
+//           <Card.Body>
+//             <Card.Title>Before Picture</Card.Title>
+//             <Card.Text>Description of the image</Card.Text>
+//           </Card.Body>
+//         </Card>
+//       </div>
+//     );
+//   }
+// }
+
+// export default GalleryCard;

0b0ce5240896d389928f899a204a8706c2666961
DevSamaa <samaakanani@gmail.com>
2020-01-31 12:17:39 +1100

    made changes to the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
+import "../styles/Footer.css";
 
 export class Footer extends Component {
   render() {
     return (
-      <div>
-        <h5>-----------------------------------------</h5>
-        <h5>Address: 21 Mulgarrah, 3088 Greensborough</h5>
-        <h5>Phone: 0414675358</h5>
-        <h6>Created by: MERN GIRLS</h6>
+      <div id="navdiv" class="bg-primary text-white">
+        <h5 class="navcontent">Address: 21 Mulgarrah, 3088 Greensborough</h5>
+        <h5 class="navcontent">Phone: 0414675358</h5>
+        <h6 class="navcontent">Created by: MERN GIRLS</h6>
       </div>
     );
   }

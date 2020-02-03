b8b3d86d965fbc62b6ec03cbc72caa8e61d2a303
DevSamaa <samaakanani@gmail.com>
2020-02-03 13:21:26 +1100

    added new gallery cards and some minor changes to footer

--- frontend/src/components/GalleryPage.js
+++ frontend/src/components/GalleryPage.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import GalleryCard from "./GalleryCard";
+import axios from "axios";
 
 export class GalleryPage extends Component {
+  state = {
+    gallery: []
+  };
+
+  async componentDidMount() {
+    const response = await axios.get("http://localhost:7070/gallery");
+    console.log(response.data);
+
+    this.setState({
+      gallery: response.data
+    });
+  }
   render() {
-    return (
-      <div>
-        <h1>Gallery</h1>
-        <GalleryCard />
-      </div>
-    );
+    if (this.state.gallery.length > 0) {
+      return (
+        <div>
+          {this.state.gallery.map(image => {
+            return <GalleryCard data={image} />;
+          })}
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 

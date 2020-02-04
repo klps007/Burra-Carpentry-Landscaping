92b529fc4690c3219e979d84b70d18afc6e894bf
DevSamaa <samaakanani@gmail.com>
2020-02-04 11:12:40 +1100

    added onSubmit message and made changes, so now the data of the quote form actually comes through on the backend.

--- backend/models/Quote.js
+++ backend/models/Quote.js
@@ -1,64 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 let quoteSchema = new mongoose.Schema(
   {
     createdTime: { type: Date, default: Date.now },
 
-    name: {
-      firstname: {
-        type: String,
-        required: true
-      },
-      lastname: {
-        type: String,
-        required: true
-      }
+    firstName: {
+      type: String,
+      required: true
     },
-    contact: {
-      phone: {
-        type: String,
-        required: true
-      },
-      email: {
-        type: String,
-        required: true
-      },
-      preferred: String
+    lastName: {
+      type: String,
+      required: true
     },
-    address: {
-      streetAddress: String,
-      suburb: String,
-      postcode: String
+    phone: {
+      type: String,
+      required: true
     },
-    particulars: {
-      commercialOrResidential: String,
-      planningOnlyOrReadyNow: String,
-      approxStartDate: Date,
-      budget$: Number,
-      needFinance: Boolean,
-      approxsizeSQM: Number
+    email: {
+      type: String,
+      required: true
     },
-    serviceRequired: [
-      {
-        paving: String,
-        patio: String,
-        retainingWall: String,
-        fencing: String,
-        decking: String,
-        poolArea: String,
-        waterFeature: String,
-        newLawn: String,
-        newPlants: String,
-        treeRemoval: String,
-        rubbishRemoval: String,
-        inHomeCarpentry: String,
-        walkInRobe: String,
-        additionalDetails: String
-      }
-    ]
+    preferred: String,
+
+    streetAddress: String,
+    suburb: String,
+    postcode: String,
+    commercialOrResidential: String,
+    planningOnlyOrReadyNow: String,
+    approxStartDate: Date,
+    budget: Number,
+    needFinance: Boolean,
+    approxsizeSQM: Number,
+    serviceRequired: []
+    // [
+    //   {
+    //     paving: String,
+    //     patio: String,
+    //     retainingWall: String,
+    //     fencing: String,
+    //     decking: String,
+    //     poolArea: String,
+    //     waterFeature: String,
+    //     newLawn: String,
+    //     newPlants: String,
+    //     treeRemoval: String,
+    //     rubbishRemoval: String,
+    //     inHomeCarpentry: String,
+    //     walkInRobe: String,
+    //     additionalDetails: String
+    //   }
+    // ]
   },
-  { collection: 'quotes' }
+  { collection: "quotes" }
 );
 
-const Quote = mongoose.model('Quote', quoteSchema);
+const Quote = mongoose.model("Quote", quoteSchema);
 module.exports = Quote;
---
+++ frontend/src/components/SubmitMessage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class SubmitMessage extends Component {
+  render() {
+    return (
+      <div>
+        <p>You're form was sent off. We'll be in touch soon.</p>
+      </div>
+    );
+  }
+}
+
+export default SubmitMessage;

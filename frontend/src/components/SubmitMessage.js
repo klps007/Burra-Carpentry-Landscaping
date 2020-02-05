c3b00ebf841813f1551039cb57de0ba81c26b414
DevSamaa <samaakanani@gmail.com>
2020-02-05 15:21:57 +1100

    changed the styling of the submit message and added bootstrap to the login form

--- frontend/src/components/SubmitMessage.js
+++ frontend/src/components/SubmitMessage.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Alert from "react-bootstrap/Alert";
 
 export class SubmitMessage extends Component {
   render() {
     return (
       <div>
-        <p>You're form was sent off. We'll be in touch soon.</p>
+        <Card className="a-gallery-card" style={{ width: "50rem" }}>
+          <Card.Body>
+            <Alert variant="primary">
+              You're form was sent off. We'll be in touch soon.
+            </Alert>
+          </Card.Body>
+        </Card>
       </div>
     );
   }

86825f4fb52ee92ccae68ac60625de22c7f8d75e
Kristy Lake <kristylake@live.com>
2020-02-06 14:00:21 +1100

    removed frontend folder from backend branch

--- frontend/cypress/integration/homepage_test.js
+++
@@ -1,6 +0,0 @@
-describe("My First Test", function() {
-  it("Visits homepage and renders the text", function() {
-    cy.visit("http://localhost:3000");
-    cy.get(".btn").click();
-  });
-});
--- frontend/cypress/integration/quote_form_test.js
+++
@@ -1,7 +0,0 @@
-describe("Testing form validation of the 'get quote' form", function() {
-  it("Button is clicked, there should be an error message", function() {
-    cy.visit("http://localhost:3000/getquote");
-    cy.get(".btn").click();
-    cy.get(":nth-child(1) > :nth-child(1) > span").should("exist");
-  });
-});
